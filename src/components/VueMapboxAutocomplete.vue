fed340006c0db3c974e4a41a1a767ddb62c0c891
quenti77 <quentin.ysambert@gmail.com>
2019-11-30 09:33:25 +0100

    master: Remove default example after create project. Load mapbox

---
+++ src/components/VueMapboxAutocomplete.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>A</div>
+</template>
+
+<script>
+export default {}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
-import App from './App.vue'
-import store from './store'
+import App from './App'
+import VueMapboxAutocomplete from '../'
+
+Vue.use(VueMapboxAutocomplete)
 
 Vue.config.productionTip = false
 
 new Vue({
-  store,
   render: h => h(App)
 }).$mount('#app')
